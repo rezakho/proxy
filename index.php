416c74c8d50e067a180098c079f22eac23383030
Reza Khodadadi <rezakho@gmail.com>
2015-06-24 00:37:18 +0430

    Some changes

--- index.php
+++ index.php
@@ -1,89 +1,148 @@
 <?php
-/*
-require('ProxyHandler.class.php');
 
-
-
-$proxy = new ProxyHandler('http://localhost','http://qlake.ir');
-$proxy->execute();
-$proxy->readResponse();
-*/
-
-function readHeaders(&$cu, $string)
+class Proxy
 {
-	$length = strlen($string);
-	if (preg_match(',^Location:,', $string)) {
-	$string = str_replace($this->proxy_url, $this->url, $string);
-	}
-	elseif(preg_match(',^Cache-Control:,', $string)) {
-	$this->cache_control = true;
-	}
-	elseif(preg_match(',^Pragma:,', $string)) {
-	$this->pragma = true;
-	}
-	if ($string !== "\r\n") {
-	header(rtrim($string));
+	protected $curl;
 
+
+
+	public function __construct($publicUrl, $privateUrl)
+	{
+		$this->curl = curl_init();
+
+		$privateUrl =  rtrim($privateUrl,'/');
+
+		if (isset($_SERVER['PATH_INFO']))
+		{
+			$privateUrl .= $_SERVER['PATH_INFO'];
+		}
+		else
+		{
+			$privateUrl .= '/';
+		}
+
+		$privateUrl .= $_SERVER['QUERY_STRING'] !== '' ? "?{$_SERVER['QUERY_STRING']}" : "";
+
+		// set URL and other appropriate options
+		curl_setopt($this->curl, CURLOPT_URL, $privateUrl);
+		curl_setopt($this->curl, CURLOPT_HEADER, 0);
+		curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($this->curl, CURLOPT_BINARYTRANSFER, 0); // For images, etc.
+		curl_setopt($this->curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+		curl_setopt($this->curl, CURLOPT_WRITEFUNCTION, [$this, 'readResponse']);
+		curl_setopt($this->curl, CURLOPT_HEADERFUNCTION, [$this, 'readHeaders']);
+
+		$this->createRequestHeaders();
 	}
-	return $length;
+
+
+
+	public function excute()
+	{
+		curl_setopt($this->curl, CURLOPT_HTTPHEADER, $this->requestHeaders);
+		curl_exec($this->curl);
+		curl_close($this->curl);
+	}
+
+
+
+	protected function readHeaders($cu, $string)
+	{
+		$length = strlen($string);
+
+		if (preg_match('#^HTTP#', $string))
+		{
+			//return $length;
+		}
+
+		if (preg_match('#^Location:#', $string))
+		{
+			//$string = str_replace($this->proxy_url, $this->url, $string);
+		}
+		elseif (preg_match('#^Cache-Control:#', $string))
+		{
+			//$this->cache_control = true;
+			//return;
+		}
+		elseif (preg_match('#^Pragma:#', $string))
+		{
+			//$this->pragma = true;
+		}
+
+		if ($string !== "\r\n")
+		{
+			if (!preg_match('#^Transfer#', $string))
+			{
+				header(rtrim($string));
+			}
+		}
+
+		return $length;
+	}
+
+
+
+	protected function readResponse(&$cu, $string)
+	{
+		 $headersParsed = true;
+
+		// Clear the Cache-Control and Pragma headers
+		// if they aren't passed from the proxy application.
+		if ($headersParsed === false)
+		{
+			if (!$this->cache_control)
+			{
+				header('Cache-Control: ');
+			}
+
+			if (!$this->pragma)
+			{
+				header('Pragma: ');
+			}
+
+			$headersParsed = true;
+		}
+
+		$length = strlen($string);
+
+		echo $string;
+
+		return $length;
+	}
+
+
+
+	protected function createRequestHeaders()
+	{
+		$headers = apache_request_headers();
+
+		foreach ($headers as $header => $value)
+		{
+			switch($header)
+			{
+				case 'Host':
+					break;
+
+				default:
+					$this->requestHeaders[] = sprintf('%s: %s', $header, $value);
+					break;
+			}
+		} 
+	}
+
 }
 
 
-function readResponse(&$cu, $string) {
-        static $headersParsed = false;
-
-        // Clear the Cache-Control and Pragma headers
-        // if they aren't passed from the proxy application.
-        if ($headersParsed === false) {
-            if (!$this->cache_control) {
-                header('Cache-Control: ');
-            }
-            if (!$this->pragma) {
-                header('Pragma: ');
-            }
-            $headersParsed = true;
-        }
-        $length = strlen($string);
-        echo $string;
-        return $length;
-    }
 
 
-$proxy_url = "http://laravel.com/";
-
-//$this->proxy_url =  rtrim($proxy_url,'/');
-
-// Parse all the parameters for the URL
-if (isset($_SERVER['PATH_INFO'])) {
-    $proxy_url .= $_SERVER['PATH_INFO'];
-}
-else {
-    // Add the '/' at the end
-    $proxy_url .= '/';
-}
-
-if ($_SERVER['QUERY_STRING'] !== '') {
-    $proxy_url .= "?{$_SERVER['QUERY_STRING']}";
-}
 
 
-// create a new cURL resource
-$ch = curl_init();
 
-// set URL and other appropriate options
-curl_setopt($ch, CURLOPT_URL, $proxy_url);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//curl_setopt($ch, CURLOPT_BINARYTRANSFER, true); // For images, etc.
-curl_setopt($ch, CURLOPT_USERAGENT,$_SERVER['HTTP_USER_AGENT']);
-//curl_setopt($ch, CURLOPT_WRITEFUNCTION, 'readResponse');
-//curl_setopt($ch, CURLOPT_HEADERFUNCTION, 'readHeaders');
 
-// grab URL and pass it to the browser
-$content  = curl_exec($ch);
+//$contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+//header('Content-Type:'.$contentType);
+//echo '<pre/>';
+//print_r(apache_request_headers());exit;
 
-$contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-header('Content-Type:'.$contentType);
-echo $content;
-// close cURL resource, and free up system resources
-curl_close($ch);
+$p = new Proxy('', 'http://tutsplus.com');
+$p->excute();
